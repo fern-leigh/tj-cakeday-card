0f5a72e20445737a26249decd1ee57f9e7f94f67
fern-leigh <111447736+fern-leigh@users.noreply.github.com>
2024-09-16 12:43:12 -0600

    add content to lily message page

--- src/routes/Basil.jsx
+++ src/routes/Basil.jsx
@@ -7,7 +7,7 @@ import { Link } from "react-router-dom";
 export default function Basil() {
     return (
         <>
-            <div className="basil-container">
+            <div className="msg-container basil">
                 <Link to="/message-index">
                     <p>--back to message index--</p>
                 </Link>
