36f9b48e004791458378219b0f989891eb387c2a
fern-leigh <111447736+fern-leigh@users.noreply.github.com>
2024-09-15 22:38:14 -0600

    add scrolling to basil page

--- src/routes/Basil.jsx
+++ src/routes/Basil.jsx
@@ -3,59 +3,84 @@ import adventures from "../assets/adventures.jpg";
 import toys from "../assets/toys-and-trash.jpg";
 import snow from "../assets/snow-dog.jpg";
 import frisbee from "../assets/frisbee-dog.mp4";
+import { Link } from "react-router-dom";
 export default function Basil() {
     return (
         <>
             <div className="basil-container">
-            <h1>To Daddy, Woof from Basil</h1>
-            <div className="msg-card">
-                <div className="msg">
-                    <p>Thank you for working hard to learn to be kind and patient
-                        with me, so I can be happy and calm! I know it's not always easy
-                        when silly things make me scared or when my
-                        stinkiness is overwhelmingly glorious! </p>
+                <Link to="/message-index">
+                    <p>--back to message index--</p>
+                </Link>
+                <h1>To Daddy, Woof from Basil</h1>
+                <div className="scroll">
+                    <a href="#patience">--next--</a>
                 </div>
-                <div className="illustration">
-                    <img src={kindness} />
+                <div className="msg-card" id="patience">
+                    <div className="msg">
+                        <p>Thank you for working hard to learn to be kind and patient
+                            with me, so I can be happy and calm! I know it's not always easy
+                            when silly things make me scared or when my
+                            stinkiness is overwhelmingly glorious! </p>
+                        <div className="scroll">
+                            <a href="#adventures">--next--</a>
+                        </div>
+                    </div>
+                    <div className="illustration">
+                        <img src={kindness} />
+                    </div>
                 </div>
-            </div>
 
-            <div className="msg-card reverse">
-                <div className="msg">
-                    <p>Thank you for taking me on lots of awesome adventures,
-                        showing me new things, and helping me not be too scared!</p>
+                <div className="msg-card reverse" id="adventures">
+                    <div className="msg">
+                        <p>Thank you for taking me on lots of awesome adventures,
+                            showing me new things, and helping me not be too scared!</p>
+                        <div className="scroll">
+                            <a href="#toys">--next--</a>
+                        </div>
+                    </div>
+                    <div>
+                        <img src={adventures} />
+                    </div>
                 </div>
-                <div>
-                    <img src={adventures} />
-                </div>
-            </div>
 
-            <div className="msg-card">
-                <div className="msg">
-                    <p>Thank you for giving me pretty cool toys and TOTALLY AMAZING trash!!</p>
+                <div className="msg-card" id="toys">
+                    <div className="msg">
+                        <p>Thank you for giving me pretty cool toys and TOTALLY AMAZING trash!!</p>
+                        <div className="scroll">
+                            <a href="#snow">--next--</a>
+                        </div>
+                    </div>
+                    <div>
+                        <img src={toys} />
+                    </div>
                 </div>
-                <div>
-                    <img src={toys} />
+                <div className="msg-card reverse" id="snow">
+                    <div className="msg">
+                        <p>And thank you for buying me this awesome house
+                            so I can play in all the snow.
+                            I know you must'a got it for me,
+                            cause Lily is too much of a wimp to go out in the cold.
+                        </p>
+                        <div className="scroll">
+                            <a href="#vid">--next--</a>
+                        </div>
+                    </div>
+                    <div>
+                        <img src={snow} />
+                    </div>
                 </div>
-            </div>
-            <div className="msg-card reverse">
-            <div className="msg">
-                <p>And thank you for buying me this awesome house
-                    so I can play in all the snow.
-                    I know you must'a got it for me,
-                    cause Lily is too much of a wimp to go out in the cold.
-                </p>
-            </div>
-            <div>
-                <img src={snow} />
-            </div>
-            </div>
-            <div className="video-container">
-                <div>
-                <p>I woof you, Daddy dog! You're totally paw-some!</p>
+                <div className="video-container" id="vid">
+                    <div>
+                        <p>I woof you, Daddy dog! You're totally paw-some!</p>
+                    </div>
+                    <div className="vid">
+                    <video src={frisbee} controls />
+                    </div>
+                    <div className="scroll">
+                   <a href="#top"> --back to top-- </a>
+                    </div>
                 </div>
-                <video src={frisbee} controls />
-            </div>
+
             </div>
         </>
     );
